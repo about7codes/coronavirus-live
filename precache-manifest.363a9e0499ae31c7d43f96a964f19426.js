07388c8a90b401a720de580d1fba5a4ea3d1bde9
about7code <sanaullahhaifz@gmail.com>
2020-04-06 21:28:57 +0400

    Updates

---
+++ precache-manifest.363a9e0499ae31c7d43f96a964f19426.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79446ecde3cc21dd159dd9a3a5599aa9",
+    "url": "/coronavirus-live/index.html"
+  },
+  {
+    "revision": "ea5adad0642a46a822fb",
+    "url": "/coronavirus-live/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "fb495c0b26d5c29616d8",
+    "url": "/coronavirus-live/static/css/main.b0d5e988.chunk.css"
+  },
+  {
+    "revision": "ea5adad0642a46a822fb",
+    "url": "/coronavirus-live/static/js/2.d6ba1781.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/coronavirus-live/static/js/2.d6ba1781.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb495c0b26d5c29616d8",
+    "url": "/coronavirus-live/static/js/main.a5c379f7.chunk.js"
+  },
+  {
+    "revision": "dda399632876ca11adc5",
+    "url": "/coronavirus-live/static/js/runtime-main.8edab890.js"
+  }
+]);
